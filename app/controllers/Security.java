ab098f74bd47b2732ccac8bbdc2641110c984163
Kostadin <kostadinvo@gmail.com>
2011-05-05 02:47:36 +0100

    Boilerplate authentication.

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.sql.*;
+import play.db.DB;
 import play.mvc.*;
 import models.*;
 
@@ -7,6 +9,50 @@ public class Security extends Secure.Security
 {
     static boolean authenticate(String username, String password)
     {
-        return true;
+        Connection c = DB.getConnection();
+        try
+        {
+            PreparedStatement stmt = c.prepareStatement("select ID from `Users` where ID = ? and PasswordHash=MD5(?)");
+            stmt.setInt(1, Integer.parseInt(username));
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            rs.beforeFirst();
+            while (rs.next())
+            {
+                return true;
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+
+    static boolean check(String profile)
+    {
+        if ((profile!=null))
+        {
+            Connection c = DB.getConnection();
+            try
+            {
+                PreparedStatement stmt = c.prepareStatement("select U.ID from `Users` as U, `UserRoles` as UR where U.ID = ? and U.UserRoleID = UR.ID and UR.Name = ?");
+                stmt.setInt(1, Integer.parseInt(connected()));
+                stmt.setString(2, profile);
+                ResultSet rs = stmt.executeQuery();
+                rs.beforeFirst();
+                while (rs.next())
+                {
+                    return true;
+                }
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
     }
 }
