e241e49a802b28e5d46f6738a667e311ec9ac5c6
Kostadin <kostadinvo@gmail.com>
2011-05-05 12:26:59 +0100

    Added chat tag.

--- app/controllers/Chat.java
+++ app/controllers/Chat.java
@@ -9,12 +9,12 @@ public class Chat extends Controller
 {
     public static class ChatMessage
     {
-        public String id;
-        public String name;
-        public String message;
-        public String timestamp;
-        
-        public ChatMessage(String id, String name, String message, String timestamp)
+        public Integer id;
+        public String  name;
+        public String  message;
+        public String  timestamp;
+
+        public ChatMessage(Integer id, String name, String message, String timestamp)
         {
             this.id = id;
             this.name = name;
@@ -22,39 +22,57 @@ public class Chat extends Controller
             this.timestamp = timestamp;
         }
     }
-    
-    public static void contents(Integer convID)
+
+    public static void contents(Integer convID, Integer fromMsgID)
     {
+        if (fromMsgID == null)
+        {
+            fromMsgID = 0;
+        }
         Connection c = DB.getConnection();
         try
         {
-            PreparedStatement stmt = c.prepareStatement("select C.ID, U.FirstName, M.Contents, M.TimeSent from `Users` as U, `Conversations` as C, `Messages` as M "+
-            "where C.ID=M.ConversationID and U.ID=M.UserID and C.ID = ?");
-            stmt.setInt(1, convID);            
+            PreparedStatement stmt = c
+                    .prepareStatement("select M.ID, U.FirstName, M.Contents, M.TimeSent from `Users` as U, `Conversations` as C, `Messages` as M "
+                            + "where C.ID=M.ConversationID and U.ID=M.UserID and C.ID = ? and M.ID>? " + "ORDER BY C.ID");
+            stmt.setInt(1, convID);
+            stmt.setInt(2, fromMsgID);
             ResultSet rs = stmt.executeQuery();
             rs.beforeFirst();
             ArrayList<ChatMessage> msgdata = new ArrayList<ChatMessage>();
+            Integer maxID = fromMsgID;
             while (rs.next())
             {
-                ChatMessage msg = new ChatMessage(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4));
+                ChatMessage msg = new ChatMessage(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+                maxID = Math.max(maxID, msg.id);
                 msgdata.add(msg);
             }
-            render(msgdata);
+            render(msgdata, maxID);
         }
         catch (SQLException e)
         {
             e.printStackTrace();
-        }        
+        }
     }
 
-    public static void view()
+    public static void view(Integer convID)
     {
-        render();
+        render(convID);
     }
     
-    public static void post(String convID, String fromMsgID, String msg)
+    public static void refreshScript(Integer convID)
     {
-        if (!Security.isConnected()) return;
+        Http.Header h = new Http.Header();
+        h.name = "Content-Type";
+        h.values.add("text/javascript");
+        Chat.response.headers.put("Content-Type", h);
+        render(convID);
+    }
+
+    public static void post(String convID, String msg)
+    {
+        if ( !Security.isConnected())
+            return;
         Connection c = DB.getConnection();
         try
         {
@@ -63,17 +81,15 @@ public class Chat extends Controller
             stmt.setInt(1, Integer.parseInt(convID));
             ResultSet rs = stmt.executeQuery();
             rs.beforeFirst();
-            if (!rs.next())
+            if ( !rs.next())
             {
                 stmt.close();
-                stmt = c.prepareStatement("INSERT INTO `Conversations`(`ID`) "+
-                        "values (?)");
+                stmt = c.prepareStatement("INSERT INTO `Conversations`(`ID`) " + "values (?)");
                 stmt.setInt(1, Integer.parseInt(convID));
-                stmt.execute();                
+                stmt.execute();
             }
             stmt.close();
-            stmt = c.prepareStatement("INSERT INTO `Messages`(`ConversationID`,`Contents`,`UserID`,`TimeSent`) "+
-                    "values (?,?,?,CURRENT_TIMESTAMP)");
+            stmt = c.prepareStatement("INSERT INTO `Messages`(`ConversationID`,`Contents`,`UserID`,`TimeSent`) " + "values (?,?,?,CURRENT_TIMESTAMP)");
             stmt.setInt(1, Integer.parseInt(convID));
             stmt.setString(2, msg);
             stmt.setInt(3, Integer.parseInt(Security.connected()));
---
+++ app/views/tags/chat/renderConversation.tag
@@ -0,0 +1,17 @@
+<div id="chat${_arg}"></div>
+<script type="text/javascript" />
+function setup${_arg}()
+{
+    refresh${_arg}();        
+    $('#sendMsgForm${_arg}').ajaxForm(function() {
+     	$('#sendMsgForm${_arg}').find('textarea[name="msg"]').val("");
+       	alert('Your message was sent successfully.');
+    });
+}
+$('#chat${_arg}').load('/chat/view?convID=${_arg}',
+function ()
+{
+	setTimeout(setup${_arg}(),100);	
+});
+</script>
+<script type="text/javascript" src="/chat/refreshScript?convID=${_arg}"></script>
