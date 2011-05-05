157a62a813bc647b84abef2debeac7139382664f
Kushal D'Souza <kushlyn@gmail.com>
2011-05-05 09:22:35 +0100

    Add/Remove

--- public/javascripts/submitArticle.js
+++ public/javascripts/submitArticle.js
@@ -8,35 +8,29 @@ $(document).ready(function(){
 	{
 		var authors = parseInt($('#numberAuthors').val());
 		var newAuthors = authors+1;
-		$('#authors').append(
-				'<div class="formHeader" id="author'+newAuthors+'">'+
-		    	'<span class="left">'+
-		    		'<input type="radio" name="author[]">'+
-		    	'</span>'+
-		    	'<span class="right">'+
-		    		'<span>'+
-		    			'<input type="text" name="firstName[]" value="First Name">'+
-		    		'</span>'+
-		    		'<span>'+
-		    			'<input type="text" name="lastName[]" value="Last Name">'+
-		    		'</span>'+
-		    		'<span>'+
-		    			'<input type="text" name="email[]" value="Email Address">'+
-		    		'</span>'+
-		    		'<span>'+
-		    			'<input type="button" class="button blue" id="remove" value="Remove">'+
-		    		'</span>'+
-		    	'</span>'+
-		    '</div>'+
-		    '<div class="spacingMedium"></div>'
-		);
+		$('#authors').append('		    <div class="formHeader" id="author'+newAuthors+'">');
+		$('#author'+newAuthors).append('		    	<span class="left">');
+		$('#author'+newAuthors+' .left').append('		    		<input type="radio" name="author[]">');
+		$('#author'+newAuthors+' .left').append('		    	</span>');
+		$('#author'+newAuthors).append('		    	<span class="right">');
+		$('#author'+newAuthors+' .right').append('		    			<input type="text" name="firstName[]" value="First Name">');
+		$('#author'+newAuthors+' .right').append('		    			<input type="text" name="lastName[]" value="Last Name">');
+		$('#author'+newAuthors+' .right').append('		    			<input type="text" name="email[]" value="Email Address">');
+		$('#author'+newAuthors+' .right').append('		    			<input type="button" class="button blue" id="remove" onClick="removeAuthor(\'author'+newAuthors+'\');" value="Remove">');
+		$('#author'+newAuthors+' .right').append('		    	</span>');
+		$('#authors').append('		    </div>');
+		$('#authors').append('		    <div class="spacingMedium"></div>');		
 		$('#numberAuthors').val(newAuthors);
 	});
 	
-	$('#remove').click(function()
-	{
-		var authorId = $(this).parent().parent().attr("id");
-		alert(authorId);
-		$('#'+authorId).remove();
-	});
+	
 });
+
+function removeAuthor(author)
+{
+	var id = '#'+author;
+	alert(id);
+	$(id).remove();
+}
+
+
