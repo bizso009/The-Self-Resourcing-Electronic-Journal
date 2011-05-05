c0d51e9fda27971454e93838acb4a80a4dbcd878
Kushal D'Souza <kushlyn@gmail.com>
2011-05-05 09:37:27 +0100

    Latest

--- public/javascripts/submitArticle.js
+++ public/javascripts/submitArticle.js
@@ -16,10 +16,10 @@ $(document).ready(function(){
 		$('#author'+newAuthors+' .right').append('		    			<input type="text" name="firstName[]" value="First Name">');
 		$('#author'+newAuthors+' .right').append('		    			<input type="text" name="lastName[]" value="Last Name">');
 		$('#author'+newAuthors+' .right').append('		    			<input type="text" name="email[]" value="Email Address">');
-		$('#author'+newAuthors+' .right').append('		    			<input type="button" class="button blue" id="remove" onClick="removeAuthor(\'author'+newAuthors+'\');" value="Remove">');
+		$('#author'+newAuthors+' .right').append('		    			<input type="button" class="button blue" id="remove" onClick="removeAuthor(\''+newAuthors+'\');" value="Remove">');
 		$('#author'+newAuthors+' .right').append('		    	</span>');
 		$('#authors').append('		    </div>');
-		$('#authors').append('		    <div class="spacingMedium"></div>');		
+		$('#authors').append('		    <div class="spacingMedium" id="spacing'+newAuthors+'"></div>');		
 		$('#numberAuthors').val(newAuthors);
 	});
 	
@@ -28,9 +28,10 @@ $(document).ready(function(){
 
 function removeAuthor(author)
 {
-	var id = '#'+author;
-	alert(id);
-	$(id).remove();
+	var authorId = '#author'+author;
+	var spacingId = '#spacing'+author
+	$(authorId).remove();
+	$(spacingId).remove();
 }
 
 
