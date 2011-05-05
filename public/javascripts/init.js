d675bc7e2eb03a1dab76b363f1b1627aed640e11
Shaabi Mohammed <hackfanatic@gmail.com>
2011-05-05 21:17:08 +0100

    commit

---
+++ public/javascripts/init.js
@@ -0,0 +1,9 @@
+jQuery.ajaxSetup({
+            dataFilter: function(data, dataType) {
+                return data;  
+            }
+        });
+
+$(document).ready(function() {
+	   // put all your jQuery goodness in here.
+});
