f91ea3b9c3112b7dd80292b70d25e11e5450d59f
Kostadin <kostadinvo@gmail.com>
2011-05-05 01:35:22 +0100

    Added "/tmp" to .gitignore, modified application.conf so it connects to the database.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,36 @@
 package controllers;
 
 import play.*;
+import play.db.DB;
 import play.mvc.*;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
-
 import models.*;
 
-public class Application extends Controller {
+public class Application extends Controller
+{
 
-    public static void index() {
-        render();
+    public static void index()
+    {
+        String myName = null;
+        Connection c = DB.getConnection();
+        try
+        {
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from `Users` where ID = 3");
+            //ResultSet rs = stmt.executeQuery("select 'Kostadin'");
+            rs.first();
+            myName = rs.getString("FirstName");
+        }
+        catch (SQLException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        render(myName);
     }
 
 }
