191d2793bb6d23aecff951e4a4cd8cda6f79bf2d
Kushal D'Souza <kushlyn@gmail.com>
2011-05-06 19:23:51 +0100

    asfasdf

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,11 @@
 package controllers;
 
-<<<<<<< HEAD
-=======
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
 import models.JournalNumber;
 import models.JournalVolume;
->>>>>>> f9f3dd96f9f99f0a0e5ed39dd2934d365b4dea12
 import play.mvc.Before;
 import play.mvc.Controller;
 
@@ -16,12 +13,8 @@ import play.mvc.Controller;
 public class Application extends Controller
 {
 
-<<<<<<< HEAD
     @SuppressWarnings("boxing")
-    @Before
-=======
 	@Before
->>>>>>> f9f3dd96f9f99f0a0e5ed39dd2934d365b4dea12
     public static void init(String userRole)
     {
     	boolean conn = Security.isConnected();
