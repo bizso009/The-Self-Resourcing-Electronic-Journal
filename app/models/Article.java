a6dc3f781ddc7601541431ed2e2caa3bbc67badd
Zsolt Bitvai <bitvaizs@gmail.com>
2011-05-06 11:58:13 +0100

    merged

--- app/models/Article.java
+++ app/models/Article.java
@@ -6,43 +6,6 @@ import play.db.jpa.*;
 import play.data.validation.*;
 
 @Entity
-<<<<<<< HEAD
-public class Article extends Model {
- 
-
-
-	public String title;
-	public Blob articlePdf;
-	public Date dateSubmitted;
-	public Date datePublished;
-	@Lob
-	public String summary;
-	public int journalNumberID;
-	public int submissionID;
-	
-    @OneToMany(mappedBy="article", cascade=CascadeType.ALL)
-	public List<PersonDetail> authorDetails; 
-    
-	@ManyToMany(mappedBy="articles",cascade=CascadeType.ALL)
-	public List<Keyword> keywords;
-	
-	public Article(String title, Blob articlePdf, Date dateSubmitted,
-			Date datePublished, String summary, int journalNumberID,
-			int submissionID) {
-		super();
-		this.title = title;
-		this.articlePdf = articlePdf;
-		this.dateSubmitted = dateSubmitted;
-		this.datePublished = datePublished;
-		this.summary = summary;
-		this.journalNumberID = journalNumberID;
-		this.submissionID = submissionID;
-	}
-
-	public Article() {
-	}
- 
-=======
 public class Article extends Model
 {
 
@@ -90,5 +53,4 @@ public class Article extends Model
     {
     }
 
->>>>>>> 0d4915537a6228799bbfb1243815a4692574015b
 }
