39d3b2b2af985313747d5d6b7bdf42f96051c40b
Kushal <kushlyn@gmail.com>
2011-05-06 23:39:55 +0100

    test

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -1,14 +1,21 @@
 package controllers;
 
+import java.util.List;
+
 import models.User;
 import play.mvc.Before;
-import play.mvc.Controller;
 
 @CRUD.For(User.class)
 public class Admin extends CRUD{
 	@Before
 	public static void init()
     {
-		Application.init("editor");
-    }    
+		Application.init(null);
+    }
+	
+	public static void index()
+	{
+		List<User> users = User.findAll();
+		render(users);
+	}
 }
