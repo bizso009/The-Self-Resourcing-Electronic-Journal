4c5ac14cf7f08802f5b0c9596ed439ff9d1f2d37
Kushal <kushlyn@gmail.com>
2011-05-07 06:14:59 +0100

    test

--- app/controllers/Subscribe.java
+++ app/controllers/Subscribe.java
@@ -11,7 +11,21 @@ public class Subscribe extends Controller{
 	
 	public static void subscribe(String firstName, String lastName, String email, String keywords, String subscription)
 	{
-		render(subscription);
+		validation.required(firstName);
+		validation.required(lastName);
+		validation.required(email);
+		validation.email(email);
+		validation.required(keywords);
+		if(validation.hasErrors())
+		{
+			params.flash();
+			validation.keep();
+			index();
+		}else
+		{
+			
+			render(firstName, lastName, email);
+		}
 	}
 
 }
