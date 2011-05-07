4e39f9d836c58e40cd8ee4b6645aee6d7f58c66e
Kostadin <kostadinvo@gmail.com>
2011-05-07 03:58:50 +0100

    Chat and view submission were enhanced

--- app/controllers/ViewSubmission.java
+++ app/controllers/ViewSubmission.java
@@ -8,6 +8,14 @@ import java.util.*;
 
 public class ViewSubmission extends Controller
 {
+    public static class ChatSettings
+    {
+        public boolean showAuthorConv;
+        public int authorRO;
+        public boolean showEditorConv;
+        public int editorRO;
+    }
+
     public static void view(Long subID)
     {
         List<Article> articles = Article.find("submission_id = ?", subID).fetch();
@@ -53,6 +61,7 @@ public class ViewSubmission extends Controller
             }
         }
         HashMap<Long, ArrayList<Review>> reviewMap = new HashMap<Long, ArrayList<Review>>();
+        HashMap<Long, ChatSettings> chatMap = new HashMap<Long, ChatSettings>();
         boolean isEditor = Security.check(UserRole.EDITOR);
         if (articles != null)
         {
@@ -67,18 +76,42 @@ public class ViewSubmission extends Controller
                     for (int j = dbReviews.size() - 1; j >= 0; j-- )
                     {
                         Review r = dbReviews.get(i);
+                        ChatSettings cs = new ChatSettings();
                         if (isEditor || isPublished)
                         {
-                            if (isEditor||(isPublished&&r.locked))
+                            if (isEditor || (isPublished && r.locked))
                             {
                                 showReviews.add(r);
+                                if (isPublished)
+                                {
+                                    cs.showAuthorConv = true;
+                                    cs.authorRO = 1;
+                                }
+                                if (isEditor)
+                                {
+                                    cs.showAuthorConv = true;
+                                    cs.authorRO = 0;
+                                    cs.showEditorConv = true;                                    
+                                }
+                                chatMap.put(r.id, cs);
                             }
                         }
                         else
                         {
-                            if (r.reviewer.id == user.id)
+                            if ((r.reviewer.id == user.id)||(r.locked&&(a.submission.author.id == user.id)))
                             {
                                 showReviews.add(r);
+                                cs.showAuthorConv = r.locked;
+                                if (r.reviewer.id == user.id)
+                                {
+                                    cs.showAuthorConv = true;
+                                    cs.showEditorConv = true;                                    
+                                }
+                                if (!Security.isConnected())
+                                {
+                                    cs.authorRO = 1;
+                                }
+                                chatMap.put(r.id, cs);
                                 break;
                             }
                         }
@@ -87,7 +120,7 @@ public class ViewSubmission extends Controller
                 reviewMap.put(a.id, showReviews);
             }
         }
-        render(user, canSelectForReview, canDownloadForReview, canCancelReview, canWriteReview, articles, reviewMap);
+        render(user, canSelectForReview, canDownloadForReview, canCancelReview, canWriteReview, articles, reviewMap, chatMap);
     }
 
     public static void download(Long id)
@@ -140,6 +173,7 @@ public class ViewSubmission extends Controller
         ra.assigned = true;
         ra.dateAssigned = new Date();
         ra.save();
-        //renderBinary(a.articlePdf.get());
+        // TODO: Enable later
+        // renderBinary(a.articlePdf.get());
     }
 }
